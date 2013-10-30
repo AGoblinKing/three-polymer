4e496a4608c4a4e7251ce9922ca074c000db70f3
Joshua Galvin <joshuagalvin@gmail.com>
2013-10-30 06:44:38 -0700

    fixed coloring

--- elements/utils.js
+++ elements/utils.js
@@ -1,12 +1,29 @@
 POLYTHREE = {
-    bindAttrib: function(from, target, name) {
-        from[name] && (target[name] = from[name]);
+    convert: function(targetValue, value) {
+        if(value.slice(0,2) === "0x") {
+            return parseInt(value.slice(2), 16);   
+        }
+        switch(typeof targetValue) {
+            case "number": 
+                value = parseFloat(value);
+                break;
+            case "boolean":
+                value = value === "true";
+                break;  
+        }
         
+        return value;
+    },
+    bindAttrib: function(from, target, name) {
+        from[name] && (target[name] = POLYTHREE.convert(target[name], from[name]));
+
         Object.defineProperty(from, name, {
             set: function(value) {
-                // should do hasOwnProperty...
-                target.needsUpdate = true;
-                target[name] = value;
+                if(target.hasOwnProperty("needsUpdate")) {
+                   target.needsUpdate = true; 
+                }
+                
+                target[name] = POLYTHREE.convert(target[name], value);
             },
             get: function() {
                 return target[name];
