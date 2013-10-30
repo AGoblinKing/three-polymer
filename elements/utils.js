b533214621d2f61b1f798ce7a51fc323a4b49f39
Joshua Galvin <joshuagalvin@gmail.com>
2013-10-30 10:20:24 -0700

    more examples

--- elements/utils.js
+++ elements/utils.js
@@ -16,7 +16,13 @@ POLYTHREE = {
     },
     bindAttrib: function(from, target, name) {
         from[name] && (target[name] = POLYTHREE.convert(target[name], from[name]));
-
+        
+        
+        // this is going to overwrite the existing property, no good.
+        // Should probably look at using "name"Changed events
+        // Though then they can diverge ... Object.observe? maybe?
+        // or... I could setup the setters/getters to look at some property
+        // and set that later
         Object.defineProperty(from, name, {
             set: function(value) {
                 if(target.hasOwnProperty("needsUpdate")) {
